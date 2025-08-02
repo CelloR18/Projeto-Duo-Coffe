b664d7455e088d5dc1784c357a97bbd9a250c2f2
CelloR18 <gustavosq2020@hotmail.com>
2025-08-02 15:29:22 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Alterna visibilidade do menu mobile
+const menuToggle = document.getElementById('menu-toggle');
+const nav = document.getElementById('nav');
+
+menuToggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
